9b8c33bb8d1058a5bf0f61add2a73f692af9c7a2
Zihao Fan <fanzihao.thu@gmail.com>
2017-03-07 18:47:14 +0800

    add flask

---
+++ service.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+from flask import Flask, request, session, render_template
+
+app = Flask(__name__)
+app_root = app.root_path
+app.config['DEBUG'] = False
+
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/participate')
+def participate():
+    return render_template('participate.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
