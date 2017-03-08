202bfd417596e356a9fb81ef59bc4e700c07a7e0
Phantom-Fan <fanzihao.thu@gmail.com>
2017-03-08 23:51:35 +0800

    static

--- service.py
+++ service.py
@@ -11,37 +11,41 @@ app.config['DEBUG'] = False
 def index():
     return render_template('homepage.html')
 
-@app.route('/<path:path>')
-def static_file(path):
-    return app.send_static_file('/templates/' + path)
+# @app.route('/<path:path>')
+# def static_file(path):
+#     return app.send_static_file('/templates/' + path)
 
-# @app.route('/news.html')
-# def news():
-#     return render_template('news.html')
+@app.route('/homepage.html')
+def homepage():
+	return render_template('homepage.html')
 
-# @app.route('/participate.html')
-# def participate():
-#     return render_template('participate.html')
+@app.route('/news.html')
+def news():
+    return render_template('news.html')
 
-# @app.route('/programs.html')
-# def programs():
-# 	return render_template('programs.html')
+@app.route('/participate.html')
+def participate():
+    return render_template('participate.html')
 
-# @app.route('/about.html')
-# def about():
-# 	return render_template('about.html')
+@app.route('/programs.html')
+def programs():
+	return render_template('programs.html')
 
-# @app.route('/about_campus.html')
-# def about_campus():
-# 	return render_template('about_campus.html')
+@app.route('/about.html')
+def about():
+	return render_template('about.html')
 
-# @app.route('/about_company.html')
-# def about_company():
-# 	return render_template('about_company.html')
+@app.route('/about_campus.html')
+def about_campus():
+	return render_template('about_campus.html')
 
-# @app.route('/about_people.html')
-# def about_people():
-# 	return render_template('about_people.html')
+@app.route('/about_company.html')
+def about_company():
+	return render_template('about_company.html')
+
+@app.route('/about_people.html')
+def about_people():
+	return render_template('about_people.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
