5836f63f5c39bd59993f66164eef811e94ee822e
Zihao Fan <fanzihao.thu@gmail.com>
2017-03-09 16:39:51 +0800

    swithed links

--- service.py
+++ service.py
@@ -7,55 +7,41 @@ app = Flask(__name__)
 app_root = app.root_path
 app.config['DEBUG'] = False
 
-
 @app.route('/')
 def index():
     return render_template('homepage.html')
-
-
-# @app.route('/<path:path>')
-# def static_file(path):
-#     return app.send_static_file('/templates/' + path)
 
 @app.route('/homepage.html')
 def homepage():
     return render_template('homepage.html')
 
-
 @app.route('/news.html')
 def news():
     return render_template('news.html')
-
 
 @app.route('/participate.html')
 def participate():
     return render_template('participate.html')
 
-
 @app.route('/programs.html')
 def programs():
     return render_template('programs.html')
-
 
 @app.route('/about.html')
 def about():
     return render_template('about.html')
 
-
 @app.route('/about_campus.html')
 def about_campus():
     return render_template('about_campus.html')
-
 
 @app.route('/about_company.html')
 def about_company():
     return render_template('about_company.html')
 
-
 @app.route('/about_people.html')
 def about_people():
     return render_template('about_people.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
